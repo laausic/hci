2c26f8576b2bb8b4353b2c217fd8bf57566d3c18
Jelena Sarić <115426213+jsaric21@users.noreply.github.com>
2024-12-18 13:56:49 +0100

    Added Michroma font

--- anubisApp/tailwind.config.ts
+++ anubisApp/tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        michroma: ['Michroma', 'sans-serif'],
+      },
     },
   },
   plugins: [],
