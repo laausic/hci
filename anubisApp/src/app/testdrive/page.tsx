981055e74b59ab6f49eac2d8f6d83124a38b88e2
“jsaric21” <jenasaric@gmail.com>
2025-02-07 18:02:50 +0100

    Test commit

--- anubisApp/src/app/testdrive/page.tsx
+++ anubisApp/src/app/testdrive/page.tsx
@@ -1,7 +1,7 @@
 export default function TestDrivePage() {
     return (
       <main className="flex min-h-screen flex-col items-center p-10">
-        <h1 className="text-6xl font-extrabold tracking-tight text-[#519078]">Schedule a Test Drive</h1>
+        <h1 className="text-6xl font-extrabold tracking-tight text-[#519078]">Schedule Test Drive</h1>
       </main>
     );
   }
