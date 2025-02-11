e97f32dbc55266fadd93435788e3a923c3e85464
“jsaric21” <jenasaric@gmail.com>
2025-02-11 21:28:18 +0100

    Added images for VIP page

--- anubisApp/src/app/purchase/page.tsx
+++ anubisApp/src/app/purchase/page.tsx
@@ -1,7 +1,7 @@
 export default function PurchasePage() {
     return (
       <main className="flex min-h-screen flex-col items-center p-10">
-        <h1 className="text-6xl font-extrabold tracking text-[#519078]">Purchase Inquiry</h1>
+        <h1 className="text-6xl font-extrabold tracking text-[#519078]">VIP</h1>
       </main>
     );
   }
